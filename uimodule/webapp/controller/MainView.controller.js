b8967360e8162b0c41361fbed284c3d740ba1eb0
dvontress73 <32649385+dvontress73@users.noreply.github.com>
2021-11-12 15:29:15 -0500

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["tutorial/products/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("tutorial.products.controller.MainView", {});
+});
